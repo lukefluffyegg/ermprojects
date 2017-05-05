45fdb116ffeac67bc27d7bee19dca9022b9392d9
Luke Davies <lukedavies@Lukes-MacBook.local>
2017-05-05 17:06:35 +0100

    commit

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -31,12 +31,15 @@
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                         @else
+                        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                        g
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
@@ -48,6 +51,7 @@
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
+
                                 </ul>
                             </li>
                         @endif
